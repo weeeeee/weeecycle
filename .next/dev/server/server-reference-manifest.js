314315d2bc5806e0a8de7e2867742cc5ea131df2
Weeecycle <steve@weeecycle.net>
2026-03-07 22:23:23 -0500

    chore: deploy workshop modal and fix deployment

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4aiN5BBp2vfgONBqh86XwDGEyCXOXn0FUaNeEWdjXJg=\"\n}"
--- .next/dev/static/chunks/src_app_contact_page_tsx_6d31fb0c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_contact_page_tsx_c811d4de._.js"
-],
-    source: "dynamic"
-});
